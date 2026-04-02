cf1a921e4e7424d06afd55c9d8f03f0ebe0d30bc
Nguyễn Phương Anh <168273913+phanhnd@users.noreply.github.com>
2026-04-02 18:36:21 +0700

    Change connection page title to 'Test thử xem như nào'

--- flutter/lib/common/widgets/connection_page_title.dart
+++ flutter/lib/common/widgets/connection_page_title.dart
@@ -11,7 +11,7 @@ Widget getConnectionPageTitle(BuildContext context, bool isWeb) {
           child: Row(
         children: [
           AutoSizeText(
-            translate('Control Remote Desktop'),
+            translate('Test thử xem như nào'),
             maxLines: 1,
             style: Theme.of(context)
                 .textTheme
